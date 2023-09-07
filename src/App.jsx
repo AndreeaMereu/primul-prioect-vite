ed76b5d179a7f15d2f31f9795e369d52ae859d95
valentin.cocorada <valentin.cocorada@metro.digital>
2023-09-07 20:50:43 +0300

    adaugare titlu pagina

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function PilatesOnline() {
+  return <h1>Bine ati venit pe pagina noastra</h1>;
+}
+
+ function App() {
+  return (
+    <div>
+      <PilatesOnline/>
+    </div>
+  );
+ }
+
+ export default App;
