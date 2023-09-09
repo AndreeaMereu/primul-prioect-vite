f2823dbd1190220e700d35a2146b501d5940bf76
valentin.cocorada <valentin.cocorada@metro.digital>
2023-09-09 18:01:14 +0300

    adaugare poza

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-function PilatesOnline() {
+const PilatesOnline = () => {
   return <h1>Bine ati venit pe pagina noastra</h1>;
 }
 
- function App() {
+const Poza1 = () => {
+return <img id="img" src="img1.jpeg" alt="pilates" width="180" height="350"/>
+}
+
+ const App = () => {
   return (
     <div>
       <PilatesOnline/>
